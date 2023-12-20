74337accad0cd49ff679fbb6a2754dbe27385536
DavidGomezC <dgomez@ibecbarcelona.eu>
2023-12-20 18:17:23 +0100

    LoadExampleExperiment

---
+++ ExampleData/20230516_144623_Water_and_DMSO_eppendorf_MAR02002_DMSO_and_water_eppendorf_CSI_reconstruction_1_1/10_CSI/tmp_img/1-MRICSIOverlayGen.py
@@ -0,0 +1,21 @@
+from PIL import Image 
+import numpy as np 
+import matplotlib.pyplot as plt 
+img = Image.open(r'D:/IBECPostDocDrive/2023_04_21_MatlabMRIDataProcessing/20230516_144623_Water_and_DMSO_eppendorf_MAR02002_DMSO_and_water_eppendorf_CSI_reconstruction_1_1/10/tmp_img/SpectraTimePoint_1.png') 
+imgMRI = Image.open(r'D:/IBECPostDocDrive/2023_04_21_MatlabMRIDataProcessing/20230516_144623_Water_and_DMSO_eppendorf_MAR02002_DMSO_and_water_eppendorf_CSI_reconstruction_1_1/10/tmp_img/MRICut.png') 
+img = img.convert("RGBA") 
+datas = img.getdata() 
+newData = [] 
+for item in datas: 
+    if item[0] == 255 and item[1] == 255 and item[2] == 255: 
+        newData.append((255, 255, 255, 0)) 
+    else: 
+        newData.append(item) 
+img.putdata(newData) 
+plt.figure(figsize=(8,8)) 
+plt.subplot(1, 1, 1) 
+plt.imshow(imgMRI.resize([i*4 for i in np.shape(imgMRI)]), cmap='gray') 
+plt.imshow(img.resize([i*4 for i in np.shape(imgMRI)])) 
+plt.xticks([]) 
+plt.yticks([]) 
+plt.savefig(r'D:/IBECPostDocDrive/2023_04_21_MatlabMRIDataProcessing/20230516_144623_Water_and_DMSO_eppendorf_MAR02002_DMSO_and_water_eppendorf_CSI_reconstruction_1_1/10/tmp_img/MRICSIOverlay_1.png', bbox_inches='tight', pad_inches=0) 
